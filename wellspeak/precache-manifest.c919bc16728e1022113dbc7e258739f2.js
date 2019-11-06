9f222e841c1821ca8cbbaa2f84c01a962bfeb9e3
Oleh Horitsyn <oleggoritsyn@cloud.upwork.com>
2019-11-06 18:42:54 +0200

    add wellspeak

---
+++ wellspeak/precache-manifest.c919bc16728e1022113dbc7e258739f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "595a82ae97e84a7400b0222716b41ea2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35c13d621afc19ea87c4",
+    "url": "/static/css/main.f8e70dfb.chunk.css"
+  },
+  {
+    "revision": "841146a859368aed6832",
+    "url": "/static/js/2.1791ec34.chunk.js"
+  },
+  {
+    "revision": "35c13d621afc19ea87c4",
+    "url": "/static/js/main.f43696ba.chunk.js"
+  },
+  {
+    "revision": "28fddcdbc408f70ae945",
+    "url": "/static/js/runtime-main.b461b54d.js"
+  }
+]);
